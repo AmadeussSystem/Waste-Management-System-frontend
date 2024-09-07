a09e84a830849a9e393a5ffe05646a71d4c3cb78
Amadeus System <amadeussystem04@gmail.com>
2024-09-07 15:14:21 +0530

    fixed svg issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const config = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+      return config;
+    },
+};
 
-export default nextConfig;
+export default config;
+  
